3f37f10337623c1388a3ec33f6f807cf5c71f728
shreyas-9 <60311883+shreyas-9@users.noreply.github.com>
2021-06-26 18:56:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Node from "./Node";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Node />
+      <Node />
+      <Node />
+      <Node />
+      <Node />
+    </div>
+  );
+}
+
+export default App;
---
+++ Node.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Node() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web React js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Node;
